a7a1d157bcc92be8677e6d92b08a7d64065b6514
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2021-09-26 02:45:59 +0200

    nav fixed, bootstrap have some problems, 40% of removing em' for px, RWD is better

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const footerYear = document.querySelector('.footer__year');
 
-
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
 
 	function addShadow() {
 		if (window.scrollY >= 300) {
@@ -12,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
+	allNavItems.forEach((item) =>
+		item.addEventListener('click', () => navList.classList.remove('show'))
+	);
+
 	window.addEventListener('scroll', addShadow);
 });
 
@@ -19,3 +24,5 @@ const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
+
+handleCurrentYear();
